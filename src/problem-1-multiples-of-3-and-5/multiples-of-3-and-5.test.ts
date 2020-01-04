28154d77bd1af6826324d8a0740bd8c326143d2c
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2020-01-04 01:20:18 -0500

    NaturalNumber can only be instantiated with an integer

--- src/problem-1-multiples-of-3-and-5/multiples-of-3-and-5.test.ts
+++ src/problem-1-multiples-of-3-and-5/multiples-of-3-and-5.test.ts
@@ -1,8 +1,22 @@
-import { NaturalNumber } from './multiples-of-3-and-5';
+import { NaturalNumber, sumOfMultiples3And5 } from './multiples-of-3-and-5';
+
+test('throws an error when trying instantiate a NaturalNumber with a float', () => {
+    expect(() => NaturalNumber.of(3.1416)).toThrow('NaturalNumber can only be instantiated with an integer.');
+});
+
 test.each([
-    [1n, 8n, [1n, 2n, 3n, 4n, 5n, 6n, 7n]],
-    [2n, 10n, [2n, 4n, 6n, 8n]],
-    [3n, 13n, [3n, 6n, 9n, 12n]],
+    [1, 8, [1, 2, 3, 4, 5, 6, 7]],
+    [2, 10, [2, 4, 6, 8]],
+    [3, 13, [3, 6, 9, 12]],
 ])('%i.findMultiplesBelow(%i)', (a, b, multiples) => {
     expect(NaturalNumber.of(a).getMultiplesBelow(b)).toEqual(multiples);
+});
+
+test.each([
+    [49, 543],
+    [19564, 89301183],
+    [8456, 16687353],
+    [1000, 233168],
+])('sumOfMultiples3and5(%i)', (a, sum) => {
+    expect(sumOfMultiples3And5(a)).toBe(sum);
 });
--- src/problem-1-multiples-of-3-and-5/multiples-of-3-and-5.ts
+++ src/problem-1-multiples-of-3-and-5/multiples-of-3-and-5.ts
@@ -1,12 +1,15 @@
 export class NaturalNumber {
-    private constructor(private integer: bigint) {}
-    static of(integer: bigint): NaturalNumber {
+    private constructor(private integer: number) {}
+    static of(integer: number): NaturalNumber {
+        if (!Number.isInteger(integer)) {
+            throw new TypeError('NaturalNumber can only be instantiated with an integer.');
+        }
         return new NaturalNumber(integer);
     }
-    getMultiplesBelow(ceiling: bigint): bigint[] {
+    getMultiplesBelow(ceiling: number): number[] {
         const multiples = [];
         let multiple = this.integer;
-        for (let i = 1n; multiple < ceiling - this.integer; i++) {
+        for (let i = 1; multiple < ceiling - this.integer; i++) {
             multiple = i * this.integer;
             multiples.push(multiple);
         }
@@ -14,3 +17,9 @@ export class NaturalNumber {
         return multiples;
     }
 }
+
+export const sumOfMultiples3And5 = (ceiling: number): number => {
+    const multiplesOf3 = NaturalNumber.of(3).getMultiplesBelow(ceiling);
+    const multiplesOf5 = NaturalNumber.of(5).getMultiplesBelow(ceiling);
+    return [...new Set([...multiplesOf3, ...multiplesOf5])].reduce((sum, current) => sum + current);
+};
