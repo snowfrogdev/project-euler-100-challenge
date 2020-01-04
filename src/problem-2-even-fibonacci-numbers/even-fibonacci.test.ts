28596331834cb22256d7a4a415b881c1b59a67e0
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2020-01-04 15:46:28 -0500

    Add test case for actual problem data

--- src/problem-2-even-fibonacci-numbers/even-fibonacci.test.ts
+++ src/problem-2-even-fibonacci-numbers/even-fibonacci.test.ts
@@ -3,4 +3,5 @@ import { evenFibonacciSum } from './evenFibonacciSum';
 test('Even Fibonacci', () => {
     expect(evenFibonacciSum(10)).toBe(10);
     expect(evenFibonacciSum(40)).toBe(44);
+    expect(evenFibonacciSum(4_000_000)).toBe(4613732);
 });
