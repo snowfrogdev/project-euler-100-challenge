89ab5189a3e377ea26830bd465bde069b8367af0
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2020-01-05 20:41:36 -0500

    feat: primeNumbers generates 2,3,5

--- src/probelm-3-largest-prime-factor/largest-prime-factor.test.ts
+++ src/probelm-3-largest-prime-factor/largest-prime-factor.test.ts
@@ -2,9 +2,11 @@ test('prime numbers generator', () => {
     const generator = primeNumbers(10);
     expect(generator.next().value).toBe(2);
     expect(generator.next().value).toBe(3);
+    expect(generator.next().value).toBe(5);
 });
 
 function* primeNumbers(max: number): Generator<number> {
     yield 2;
     yield 3;
+    yield 5;
 }
