ca538dc24b4336197fc73b55d8ae7010023d6aee
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2020-01-05 20:51:54 -0500

    test(primeNumbers): make test use array of results

--- src/probelm-3-largest-prime-factor/largest-prime-factor.test.ts
+++ src/probelm-3-largest-prime-factor/largest-prime-factor.test.ts
@@ -1,8 +1,9 @@
 test('prime numbers generator', () => {
-    const generator = primeNumbers(10);
-    expect(generator.next().value).toBe(2);
-    expect(generator.next().value).toBe(3);
-    expect(generator.next().value).toBe(5);
+    const primes = [...primeNumbers(10)];
+
+    const expected = [2, 3, 5,]// 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+    expect(primes).toEqual(expected);
 });
 
 function* primeNumbers(max: number): Generator<number> {
