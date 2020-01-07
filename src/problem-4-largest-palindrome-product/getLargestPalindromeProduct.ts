6466d902efb0ab1b815321868c22602b1a0b840c
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2020-01-06 20:54:34 -0500

    refactor(problem 4): extract solution to separate file.

---
+++ src/problem-4-largest-palindrome-product/getLargestPalindromeProduct.ts
@@ -0,0 +1,25 @@
+function isPalindrome(num: number): boolean {
+    const reversedNumber = Number(
+        num
+            .toString()
+            .split('')
+            .reverse()
+            .join(''),
+    );
+    return reversedNumber === num;
+}
+
+export function getLargestPalindromeProduct(digitsOfFactors: number): number {
+    const highestFactor = 10 ** digitsOfFactors - 1;
+    const lowestFactor = 10 ** (digitsOfFactors - 1);
+    let largestPalindrome = 0;
+    for (let i = lowestFactor; i <= highestFactor; i++) {
+        for (let j = lowestFactor; j <= highestFactor; j++) {
+            const product = i * j;
+            if (isPalindrome(product) && product > largestPalindrome) {
+                largestPalindrome = product;
+            }
+        }
+    }
+    return largestPalindrome;
+}
--- src/problem-4-largest-palindrome-product/largest-palindrome-product.test.ts
+++ src/problem-4-largest-palindrome-product/largest-palindrome-product.test.ts
@@ -1,34 +1,6 @@
+import { getLargestPalindromeProduct } from './getLargestPalindromeProduct';
+
 test('getLargestPalindromeProduct()', () => {
     expect(getLargestPalindromeProduct(2)).toBe(9009);
     expect(getLargestPalindromeProduct(3)).toBe(906609);
 });
-
-function getLargestPalindromeProduct(digitsOfFactors: number): number {
-    const highestFactor = 10 ** digitsOfFactors - 1;
-    const lowestFactor = 10 ** (digitsOfFactors - 1);
-
-    let largestPalindrome = 0;
-
-    for (let i = lowestFactor; i <= highestFactor; i++) {
-        for (let j = lowestFactor; j <= highestFactor; j++) {
-            const product = i * j;
-            if (isPalindrome(product) && product > largestPalindrome) {
-                largestPalindrome = product;
-            }
-        }
-    }
-
-    return largestPalindrome;
-}
-
-function isPalindrome(num: number): boolean {
-    const reversedNumber = Number(
-        num
-            .toString()
-            .split('')
-            .reverse()
-            .join(''),
-    );
-
-    return reversedNumber === num;
-}
