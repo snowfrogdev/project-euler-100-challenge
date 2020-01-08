5c6f1f14dc616c43add1dc01f24bfc2883871aca
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2020-01-07 22:55:52 -0500

    refactor(problem 4): performance optimization
    Instead of checking every integer, only check muliples of max.

--- src/problem-5-smallest-multiple/getSmallestMultiple.ts
+++ src/problem-5-smallest-multiple/getSmallestMultiple.ts
@@ -1,13 +1,15 @@
 export function getSmallestMultiple(max: number): number {
-    for (let i = 1; i < Number.MAX_SAFE_INTEGER; i++) {
+    for (let i = max; i <= Number.MAX_SAFE_INTEGER; i += max) {
         for (let j = 1; j <= max; j++) {
             if (i % j !== 0) {
                 break;
             }
+
             if (j === max) {
                 return i;
             }
         }
     }
+
     return 0;
 }
